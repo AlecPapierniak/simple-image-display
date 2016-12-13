dc25a4e05949c8a1c991516bdf8eb3e972f259b0
Alec Papierniak <alec@papierniak.net>
2016-12-13 09:14:48 -0600

    creating array to hold filenames and sorting on filename

--- index.php
+++ index.php
@@ -12,20 +12,30 @@
 
 <?php
     $dir = new DirectoryIterator(dirname(__FILE__));
+    $images = array();
 
+    //build the array of files
     foreach ($dir as $fileinfo) {
         if (!$fileinfo->isDot()) {
             if ($fileinfo->getExtension() == "jpg"){
-                ?>
-                <div>
-                    <a href=<?php echo $fileinfo->getFilename(); ?>>
-                        <img src=<?php echo $fileinfo->getFilename(); ?> />
-                    </a>
-                </div>
-                <?php
+                array_push($images, $fileinfo->getFilename());
             }
         }
     }
+
+    //sort
+    sort($images);
+
+    //display
+    foreach ($images as $image) {
+        ?>
+        <div>
+            <a href=<?php echo $image; ?>>
+                <img src=<?php echo $image; ?> />
+            </a>
+        </div>
+        <?php
+    }    
 ?>
 
 </body>
